6958b503c61c317d2915e31c85da2b4afb517653
dr033 <fanssilva06@gmail.com>
2022-08-10 23:07:39 +0100

    Updated intro text

--- JS/resume_text.js
+++ JS/resume_text.js
@@ -1,12 +1,4 @@
-var text = "My name is Filipe Silva, I'm 24 years old and I'm from Albufeira-Algarve-Portugal. " +
-    "I recently finished my degree in Computer Engineering, in the field of application development," +
-    " at Instituto Superior de Engenharia de Coimbra. Course whose objective is to prepare its students " +
-    " to serve effectively and professionally in the field of Information Technology. " +
-    "I consider myself a perfectionist, which combined with my persistent essence, can benefit me " +
-    "in projects, companies and people. I adapt easily to all circumstances, having the pragmatic " +
-    "ability to stop, think and resolve situations quickly and effectively. I believe I am skilled " +
-    "in managing personal relationships, as well as being able to work efficiently in a team. In this sense," +
-    " I believe that my values, skills and abilities will guide me towards an adequate and efficient performance.";
+var text = "My name is Filipe Silva, I'm 24 years old, and I'm from Albufeira-Algarve-Portugal. I ended up graduating with a degree in Computer Engineering, in the area of application development, at the Higher Institute of Engineering of Coimbra (ISEC). A course whose objective is to prepare its students to act effectively and professionally in the IT area. I consider myself a perfectionist, which combined with my persistent essence, can benefit me in projects, companies and people. I adapt easily to all circumstances, having the pragmatic ability to stop, think and resolve situations quickly and effectively. I believe that I am skilled in stress management, in addition to being able to work efficiently in a team. In this sense, I believe that my values, skills and abilities will guide me to adequate and efficient performance.";
 document.getElementById("text").innerHTML += text;
 var text = "Filipe A. N. Silva"
 //p.textContent = text
