65dc311cfcab7ffb64167ffd28b779740d0279e6
Dr33_LinuxDesktop <fanssilva06@gmail.com>
2022-11-06 17:07:28 +0000

    Changed css, removed some info and added age effect to presentation

--- templates/index/JS/index.js
+++ templates/index/JS/index.js
@@ -1,51 +1,57 @@
-var age = getAge("1997-08-16");
-
-var text =
-  "My name is Filipe Silva, I'm " +
-  age +
-  ' years old, and I\'m from <a href="https://goo.gl/maps/r4pW8UuQMEJsNjTKA" target="_blank">Albufeira - Portugal</a>. I am graduated in Computer Engineering, ' +
-  "at the Higher Institute of Engineering of Coimbra (ISEC). A course whose objective is to prepare its students to act effectively and professionally " +
-  "in the IT area.<p> I consider myself a perfectionist, which combined with my persistent essence. I adapt easily to all circumstances, having the " +
-  "pragmatic ability to stop, think and resolve situations quickly and effectively. I believe that I am skilled in stress management, in addition " +
-  "to being able to work efficiently in a team. In this sense, I believe that my values, skills and abilities will guide me to adequate and efficient performance.";
-document.getElementById("text").innerHTML += text;
-
-text = "Filipe A. N. Silva";
-
-var i = 0;
-function typing() {
-  if (i < text.length) {
-    document.getElementById("title").innerHTML += text.charAt(i);
-    i++;
-    if (text.charAt(i) == " ") setTimeout(typing, 200);
-    else setTimeout(typing, 100);
-  }
-}
-typing();
-
 function getAge(dateString) {
-  var ageInMilliseconds = new Date() - new Date(dateString);
+  let ageInMilliseconds = new Date() - new Date(dateString);
   return Math.floor(ageInMilliseconds / 1000 / 60 / 60 / 24 / 365); // convert to years
 }
+let age = getAge('1997-08-16');
+document.getElementById('age').innerHTML = age;
+
+let i = 0;
+let intervalTimer = 2;
+let counterTimeout;
+function ageEffect(i) {
+  if (i < age) {
+    document.getElementById('age').innerHTML = i++;
+    intervalTimer *= 1.3;
+    counterTimeout = setTimeout(ageEffect, intervalTimer, i);
+  } else {
+    document.getElementById('age').innerHTML = i;
+    document.getElementById('age').style.textDecoration = 'underline';
+
+    clearTimeout(counterTimeout);
+  }
+}
+ageEffect(i);
+
+i = 0;
+function typingTitle() {
+  text = 'Filipe A. N. Silva';
+  if (i < text.length) {
+    document.getElementById('title').innerHTML += text.charAt(i++);
+    if (text.charAt(i) == ' ') {
+      setTimeout(typingTitle, 200);
+    } else setTimeout(typingTitle, 100);
+  }
+}
+typingTitle();
 
 function isIOS() {
-  var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  let isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
   console.log(isIOS);
 
   if (isIOS == true) {
-    var elems = document.getElementsByClassName("presentation");
-    for (var i = 0; i < elems.length; i++) {
-      elems[i].style.backgroundAttachment = "scroll";
+    let elems = document.getElementsByClassName('presentation');
+    for (let i = 0; i < elems.length; i++) {
+      elems[i].style.backgroundAttachment = 'scroll';
     }
 
-    elems = document.getElementsByClassName("education");
-    for (var i = 0; i < elems.length; i++) {
-      elems[i].style.backgroundAttachment = "scroll";
+    elems = document.getElementsByClassName('education');
+    for (let i = 0; i < elems.length; i++) {
+      elems[i].style.backgroundAttachment = 'scroll';
     }
 
-    elems = document.getElementsByClassName("work");
-    for (var i = 0; i < elems.length; i++) {
-      elems[i].style.backgroundAttachment = "scroll";
+    elems = document.getElementsByClassName('work');
+    for (let i = 0; i < elems.length; i++) {
+      elems[i].style.backgroundAttachment = 'scroll';
     }
   }
 }
